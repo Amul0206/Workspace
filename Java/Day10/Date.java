e4dfe883cd6dac1efe4b154ed104c9b669ebf7cf
Amul0206 <amitmehul1@gmail.com>
2025-03-10 09:12:36 +0530

    commit

--- Java/Day10/Date.java
+++ Java/Day10/Date.java
@@ -4,22 +4,30 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.Period;
 import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAdjusters;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-@SuppressWarnings("unused")
+// @SuppressWarnings("unused")
 public class Date {
     public static void main(String[] args) {
         LocalDate today = LocalDate.now();
         System.out.println(today);
 
+        System.out.println();
+
         LocalDate diwali = LocalDate.of(2025, 10, 20);
         System.out.println(diwali);
 
+        System.out.println(
+
+        );
         Period tillDiwali = today.until(diwali);
         // System.out.println(tillDiwali);
         System.out.println("Time left for Diwali: " + tillDiwali.getMonths() + " months " + tillDiwali.getDays() + " days");
+
+        System.out.println(); 
 
         LocalTime now = LocalTime.now();
         System.out.println(now);
@@ -27,13 +35,21 @@ public class Date {
         long timeToDiwali = today.until(diwali, ChronoUnit.DAYS);
         System.out.println("Time left for Diwali: " + timeToDiwali + " days");
 
-    //     Stream<LocalDate> allyear = LocalDate.of(0, 0, 0).datesUntil(LocalDate.of(0, 0, 0));
+        System.out.println();
+
+        Stream<LocalDate> allyear = LocalDate.of(2025, 1, 1).datesUntil(LocalDate.of(2026, 1, 1));
         
-    //     List<LocalDate> blackfridays = allyear.filter(date -> date.getDayOfWeek() == DayOfWeek.FRIDAY && date.getDayOfMonth() == 13)
-    //         .collect(Collectors.toList());
+        List<LocalDate> blackfridays = allyear.filter(date -> date.getDayOfWeek() == DayOfWeek.FRIDAY && date.getDayOfMonth() == 13)
+            .collect(Collectors.toList());
 
-    //     System.out.println(blackfridays);
+        System.out.println(blackfridays);
 
-    // }
-}
+        System.out.println("___________________");
+        System.out.println();
+        System.out.println(diwali.with(TemporalAdjusters.firstDayOfNextMonth()));
+        System.out.println(diwali.with(TemporalAdjusters.nextOrSame(DayOfWeek.WEDNESDAY)));
+        System.out.println(diwali.with(TemporalAdjusters.previous(DayOfWeek.MONDAY)));
+        System.out.println(diwali.with(TemporalAdjusters.lastDayOfMonth()));
+        System.out.println(diwali.with(TemporalAdjusters.lastInMonth(DayOfWeek.SUNDAY)));
+    }
 }
